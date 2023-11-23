7428867310cc40707b98594e76c38329fb23823d
Dheeraj Rajput <dheerajrajput1232@gmail.com>
2023-11-24 01:13:40 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -30,20 +30,6 @@ app.use('/api/', addQuestion);
 //     res.send(questions);
 // })
 
-// -------------------------------Deployment---------------
-
-const __dirname1 = path.resolve();
-
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname1,"/Frontend/build")));
-
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname1,"Frontend","build","index.html"));
-    })
-}else{
-        // res.send("API is Running Successfully");
-    
-}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT ,console.log("Server started on PORT", PORT))
